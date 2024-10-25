6633e29fa3f675ab89a984dfa92faec63767c7f1
Tommy Zieba <tommy.zieba@mohawkcollege.ca>
2024-10-25 09:23:43 -0400

    Temporary changes to extend details page with editing functionality and more fields.

--- src/pages/TimeSheetDetails.vue
+++ src/pages/TimeSheetDetails.vue
@@ -9,6 +9,17 @@
     <div v-if="!isEditing">
       <p>Date Range: {{ timeSheet.dateRange }}</p>
       <p>Hours Worked: {{ timeSheet.hoursWorked }}</p>
+      
+      <h2>Entries</h2>
+      <div v-for="entry in timeSheet.entries" :key="entry.id">
+        <p>Date: {{ entry.date }}</p>
+        <p>Work Order: {{ entry.workOrder }}</p>
+        <p>Customer Name: {{ entry.customerName }}</p>
+        <p>Billable Rate 1ST: {{ entry.billable.rate1ST }}</p>
+        <p>Non-Billable Rate 2ST: {{ entry.nonBillable.rate2ST }}</p>
+        <p>Per Diem: {{ entry.additionalQualifiers.perDiem }}</p>
+        <p>Day/Night: {{ entry.additionalQualifiers.dayOrNight }}</p>
+      </div>
     </div>
 
     <div v-else>
@@ -20,6 +31,39 @@
         Hours Worked:
         <input v-model="editedTimeSheet.hoursWorked" type="number" />
       </label>
+
+      <h2>Edit Entries</h2>
+      <div v-for="(entry, index) in editedTimeSheet.entries" :key="entry.id">
+        <label>
+          Date:
+          <input v-model="entry.date" type="date" />
+        </label>
+        <label>
+          Work Order:
+          <input v-model="entry.workOrder" type="text" />
+        </label>
+        <label>
+          Customer Name:
+          <input v-model="entry.customerName" type="text" />
+        </label>
+        <label>
+          Billable Rate 1ST:
+          <input v-model="entry.billable.rate1ST" type="number" />
+        </label>
+        <label>
+          Non-Billable Rate 2ST:
+          <input v-model="entry.nonBillable.rate2ST" type="number" />
+        </label>
+        <label>
+          Per Diem:
+          <input v-model="entry.additionalQualifiers.perDiem" type="number" />
+        </label>
+        <label>
+          Day/Night:
+          <input v-model="entry.additionalQualifiers.dayOrNight" type="number" />
+        </label>
+      </div>
+
       <button @click="saveChanges">Save</button>
     </div>
 
